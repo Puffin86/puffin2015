30b5b12af9a5a14b9d0aa43bbed7e923ad047910
Puffin86 <511635157@qq.com>
2015-06-07 21:25:42 +0800

    dd

--- sszx/src/main/java/com/bsoft/sszx/util/Pdf_zjqd.java
+++ sszx/src/main/java/com/bsoft/sszx/util/Pdf_zjqd.java
@@ -3,6 +3,7 @@ package com.bsoft.sszx.util;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
@@ -40,31 +41,196 @@ public class Pdf_zjqd {
 	      Font headFont = new Font(bfChinese, 10, Font.BOLD);//设置字体大小 
 	      Font headFont1 = new Font(bfChinese, 8, Font.BOLD);//设置字体大小 
 	      Font headFont2 = new Font(bfChinese, 10, Font.NORMAL);//设置字体大小 
-	      float[] widths = {140f, 60f, 320f, 120f, 110f, 110f, 190f};//设置表格的列宽
+	      float[] widths = {60f, 160f, 110f, 110f, 110f};//设置表格的列宽
 	      PdfPTable table = new PdfPTable(widths);//建立一个pdf表格
 	     table.setSpacingBefore(130f);//设置表格上面空白宽度
 	   table.setTotalWidth(535);//设置表格的宽度             
 	   table.setLockedWidth(true);//设置表格的宽度固定
 	   //table.getDefaultCell().setBorder(0);//设置表格默认为无边框
-	   PdfPCell cell = new PdfPCell(new Paragraph("Taony125 testPdf 中文字体",headFont));//建立一个单元格
+	   PdfPCell cell = new PdfPCell(new Paragraph("诉讼材料转接清单",headFont));//建立一个单元格
 	   //cell.setBorder(0);//设置单元格无边框
-	   cell.setColspan(7);//设置合并单元格的列数
+	   cell.setColspan(5);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   cell.setBackgroundColor(BaseColor.GRAY);
 	   table.addCell(cell);//增加单元格
-	   cell = new PdfPCell(new Paragraph("Taony125 testPdf 中文字体",headFont));
+	   
+	   cell = new PdfPCell(new Paragraph("编号",headFont));
 	      //cell.setBorder(0);
 	   cell.setFixedHeight(20);
-	   cell.setColspan(7);//设置合并单元格的列数
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   cell.setMinimumHeight(60f);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("案号",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
 	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
 	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 	   table.addCell(cell);
-	   cell = new PdfPCell(new Paragraph("Taony125 testPdf 中文字体",headFont1));
+	   
+	   cell = new PdfPCell(new Paragraph("承办人",headFont));
 	      //cell.setBorder(0);
 	   cell.setFixedHeight(20);
-	   cell.setColspan(7);//设置合并单元格的列数
+	   //cell.setColspan(2);//设置合并单元格的列数
 	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
 	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 	   table.addCell(cell);
-	   table.addCell(new Paragraph("Taony125 testPdf 中文字体",headFont2));
+	   
+	   cell = new PdfPCell(new Paragraph("日期",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("当事人联系电话",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   //---------------------------------
+	   
+	   cell = new PdfPCell(new Paragraph("1",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("2",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("3",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("4",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("12345",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   
+	   
+	   //--------------------------------------
+	   cell = new PdfPCell(new Paragraph("材料名称",headFont1));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   cell.setColspan(3);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("页数",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("份数",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   //-----------------------------------------
+	   
+	   cell = new PdfPCell(new Paragraph("材料1",headFont1));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   cell.setColspan(3);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("1",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   cell = new PdfPCell(new Paragraph("2",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   //-----------------------------------------
+	   
+	   
+	   cell = new PdfPCell(new Paragraph("转接人",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   
+	   cell = new PdfPCell(new Paragraph("AAAA",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   
+	   cell = new PdfPCell(new Paragraph("当事人签名",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   //cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   
+	   cell = new PdfPCell(new Paragraph("",headFont));
+	      //cell.setBorder(0);
+	   cell.setFixedHeight(20);
+	   cell.setColspan(2);//设置合并单元格的列数
+	   cell.setHorizontalAlignment(Element.ALIGN_CENTER);//设置内容水平居中显示
+	   cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+	   table.addCell(cell);
+	   
+	   
+	   //table.addCell(new Paragraph("Taony125 testPdf 中文字体",headFont2));
 	   document.add(table);
 
 	     } catch (DocumentException de) {
