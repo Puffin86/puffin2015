ab9fec3fd8373c165523e99b89847de461d149c8
Puffin86 <511635157@qq.com>
2015-06-22 22:57:26 +0800

    sms

--- sszx/src/main/java/com/bsoft/sszx/xfireclient/SMSClient.java
+++ sszx/src/main/java/com/bsoft/sszx/xfireclient/SMSClient.java
@@ -4,7 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-import org.codehaus.xfire.client.Client;
+//import org.codehaus.xfire.client.Client;
 
 import com.bsoft.sszx.entity.sms.SmsBean;
 
@@ -20,79 +20,79 @@ public class SMSClient {
 	 * 发送短信
 	 * @return
 	 */
-	public boolean sendSMS(List<SmsBean> smsList){
-		boolean serviceFlag = false;
-		
-		if(smsList==null || smsList.size()==0){
-			return serviceFlag;
-		}
-		
-		try {
-//			http://203.0.64.68/dxpt/services/SendSMS?wsdl
-			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl"));
-			
-			Object[][] smsArr = new Object[smsList.size()][10];
-			for(int i =0;i<smsList.size();i++){
-				SmsBean bean = smsList.get(i);
-				smsArr[i][0] = bean.getXtbh();
-				smsArr[i][1] = bean.getXtbh();
-				smsArr[i][2] = bean.getXtbh();
-				smsArr[i][3] = bean.getXtbh();
-				smsArr[i][4] = bean.getXtbh();
-				smsArr[i][5] = bean.getXtbh();
-				smsArr[i][6] = bean.getXtbh();
-				smsArr[i][7] = bean.getXtbh();
-				smsArr[i][8] = bean.getXtbh();
-				smsArr[i][9] = bean.getXtbh();
-			}
-			Object[] result1 = client.invoke("sendMessage", smsArr); 
-			System.out.println(result1[0]); 
-			serviceFlag = true;
-		} catch (MalformedURLException e) { 
-			e.printStackTrace(); 
-		} catch (Exception e) { 
-			e.printStackTrace(); 
-		}finally{
-			return true;
-		}
-		
-	}
-	
-	/**
-	 * 查询短信状态
-	 */
-	public void searchSMSZT(){
-		try {
-//			http://203.0.64.68/dxpt/services/SendSMS?wsdl
-			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SearchSMS?wsdl")); 
-			Object[] result1 = client.invoke("getFszt", new Object[] {1, 2}); 
-			System.out.println(result1[0]); 
-		} catch (MalformedURLException e) { 
-			e.printStackTrace(); 
-		} catch (Exception e) { 
-			e.printStackTrace(); 
-		}
-		
-	}
-	
-	/**
-	 * 删除未发送短信
-	 * @return
-	 */
-	public boolean delSMS(){
-		boolean serviceFlag = false;
-		try {
-			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl")); 
-			Object[] result1 = client.invoke("delSMS", new Object[] {1, 2}); 
-			System.out.println(result1[0]); 
-			serviceFlag = true;
-		} catch (MalformedURLException e) { 
-			e.printStackTrace(); 
-		} catch (Exception e) { 
-			e.printStackTrace(); 
-		}finally{
-			return true;
-		}
-	}
+//	public boolean sendSMS(List<SmsBean> smsList){
+//		boolean serviceFlag = false;
+//		
+//		if(smsList==null || smsList.size()==0){
+//			return serviceFlag;
+//		}
+//		
+//		try {
+////			http://203.0.64.68/dxpt/services/SendSMS?wsdl
+//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl"));
+//			
+//			Object[][] smsArr = new Object[smsList.size()][10];
+//			for(int i =0;i<smsList.size();i++){
+//				SmsBean bean = smsList.get(i);
+//				smsArr[i][0] = bean.getXtbh();
+//				smsArr[i][1] = bean.getXtbh();
+//				smsArr[i][2] = bean.getXtbh();
+//				smsArr[i][3] = bean.getXtbh();
+//				smsArr[i][4] = bean.getXtbh();
+//				smsArr[i][5] = bean.getXtbh();
+//				smsArr[i][6] = bean.getXtbh();
+//				smsArr[i][7] = bean.getXtbh();
+//				smsArr[i][8] = bean.getXtbh();
+//				smsArr[i][9] = bean.getXtbh();
+//			}
+//			Object[] result1 = client.invoke("sendMessage", smsArr); 
+//			System.out.println(result1[0]); 
+//			serviceFlag = true;
+//		} catch (MalformedURLException e) { 
+//			e.printStackTrace(); 
+//		} catch (Exception e) { 
+//			e.printStackTrace(); 
+//		}finally{
+//			return true;
+//		}
+//		
+//	}
+//	
+//	/**
+//	 * 查询短信状态
+//	 */
+//	public void searchSMSZT(){
+//		try {
+////			http://203.0.64.68/dxpt/services/SendSMS?wsdl
+//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SearchSMS?wsdl")); 
+//			Object[] result1 = client.invoke("getFszt", new Object[] {1, 2}); 
+//			System.out.println(result1[0]); 
+//		} catch (MalformedURLException e) { 
+//			e.printStackTrace(); 
+//		} catch (Exception e) { 
+//			e.printStackTrace(); 
+//		}
+//		
+//	}
+//	
+//	/**
+//	 * 删除未发送短信
+//	 * @return
+//	 */
+//	public boolean delSMS(){
+//		boolean serviceFlag = false;
+//		try {
+//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl")); 
+//			Object[] result1 = client.invoke("delSMS", new Object[] {1, 2}); 
+//			System.out.println(result1[0]); 
+//			serviceFlag = true;
+//		} catch (MalformedURLException e) { 
+//			e.printStackTrace(); 
+//		} catch (Exception e) { 
+//			e.printStackTrace(); 
+//		}finally{
+//			return true;
+//		}
+//	}
 	
 }
