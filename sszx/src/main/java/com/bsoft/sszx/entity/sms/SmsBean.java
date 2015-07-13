30b8e04249f1988d00c20720fc8fefd74fe748c6
Puffin86 <511635157@qq.com>
2015-07-13 22:01:20 +0800

    bug

--- sszx/src/main/java/com/bsoft/sszx/entity/sms/SmsBean.java
+++ sszx/src/main/java/com/bsoft/sszx/entity/sms/SmsBean.java
@@ -1,16 +1,28 @@
 package com.bsoft.sszx.entity.sms;
 
 public class SmsBean {
-	private String xtbh="023";//系统编号
-	private String fsr_fybm = "1303";//法院编码
-	private String fsr_userid="1303-zuh";//发送人ID 1300-linjd OA中获取
-	private String jssjhm="13655710068";//接收手机号码；
-	private String jsrmc="AAA";//接收人名称
-	private String fsnr="nrnrnrnr";//发送内容
-	private Object dsfssj=null;//格式2013-11-07 20：02：02 null马上发送
-	private Object fssm=null;//发送说明 可NULL
-	private String id2="1234";//
-	private String ah="";//案号
+	
+//	private String xtbh="023";//系统编号 （必输项）
+//	private String fsr_fybm = "1303";//发送人法院编码（必输项）
+//	private String fsr_userid="1303-zuh";//发送人Userid（必输项，格式为1300-linjd,为从OA系统获取过来的值）
+//	private String jssjhm="13655710068";//接收人手机号码（必输项）
+//	private String jsrmc="AAA";//接收人名称（必输项）
+//	private String fsnr="nrnrnrnr";//发送内容（必输项）
+//	private String dsfssj=null;//定时发送时间（传大于当前的时间，需立即发送时传空即可），格式为2013-11-07 20:02:00
+//	private String fssm=null;//发送说明（可传空）
+//	private String id2="1234";//业务系统短信息唯一ID号(建议设为Ip+含有时分秒的时间+批量发送时的递增号，例如：2031331302242013102907283979239；必输项；最长不能超过45位)
+//	private String ah="";//案号（审判、执行等系统需传，没有涉及到“案号”的系统在传参时直接传空即可）
+	
+	private String xtbh="023";//系统编号 （必输项）
+	private String fsr_fybm = "1303";//发送人法院编码（必输项）
+	private String fsr_userid="";//发送人Userid（必输项，格式为1300-linjd,为从OA系统获取过来的值）
+	private String jssjhm="";//接收人手机号码（必输项）
+	private String jsrmc="";//接收人名称（必输项）
+	private String fsnr="";//发送内容（必输项）
+	private String dsfssj=null;//定时发送时间（传大于当前的时间，需立即发送时传空即可），格式为2013-11-07 20:02:00
+	private String fssm=null;//发送说明（可传空）
+	private String id2="";//业务系统短信息唯一ID号(建议设为Ip+含有时分秒的时间+批量发送时的递增号，例如：2031331302242013102907283979239；必输项；最长不能超过45位)
+	private String ah="";//案号（审判、执行等系统需传，没有涉及到“案号”的系统在传参时直接传空即可）
 	public String getXtbh() {
 		return xtbh;
 	}
@@ -47,16 +59,16 @@ public class SmsBean {
 	public void setFsnr(String fsnr) {
 		this.fsnr = fsnr;
 	}
-	public Object getDsfssj() {
+	public String getDsfssj() {
 		return dsfssj;
 	}
-	public void setDsfssj(Object dsfssj) {
+	public void setDsfssj(String dsfssj) {
 		this.dsfssj = dsfssj;
 	}
-	public Object getFssm() {
+	public String getFssm() {
 		return fssm;
 	}
-	public void setFssm(Object fssm) {
+	public void setFssm(String fssm) {
 		this.fssm = fssm;
 	}
 	public String getId2() {
--- sszx/src/main/java/com/bsoft/sszx/xfireclient/SMSClient.java
+++ sszx/src/main/java/com/bsoft/sszx/xfireclient/SMSClient.java
@@ -4,95 +4,99 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-//import org.codehaus.xfire.client.Client;
-
 import com.bsoft.sszx.entity.sms.SmsBean;
 
+import dxpt.Client.SearchSMSClient;
+import dxpt.Client.SendSMSClient;
+
 public class SMSClient {
-	
 	
 	private String xtbh="";//系统编号
 	private String fsr_fybm = "";//法院编码
 	private String fsr_userid="";//发送人ID 1300-linjd OA中获取
 	private String jssjhm="";//接收手机号码；
 	
+	
 	/**
 	 * 发送短信
 	 * @return
 	 */
-//	public boolean sendSMS(List<SmsBean> smsList){
-//		boolean serviceFlag = false;
-//		
-//		if(smsList==null || smsList.size()==0){
-//			return serviceFlag;
-//		}
-//		
-//		try {
-////			http://203.0.64.68/dxpt/services/SendSMS?wsdl
-//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl"));
-//			
-//			Object[][] smsArr = new Object[smsList.size()][10];
-//			for(int i =0;i<smsList.size();i++){
-//				SmsBean bean = smsList.get(i);
-//				smsArr[i][0] = bean.getXtbh();
-//				smsArr[i][1] = bean.getXtbh();
-//				smsArr[i][2] = bean.getXtbh();
-//				smsArr[i][3] = bean.getXtbh();
-//				smsArr[i][4] = bean.getXtbh();
-//				smsArr[i][5] = bean.getXtbh();
-//				smsArr[i][6] = bean.getXtbh();
-//				smsArr[i][7] = bean.getXtbh();
-//				smsArr[i][8] = bean.getXtbh();
-//				smsArr[i][9] = bean.getXtbh();
-//			}
-//			Object[] result1 = client.invoke("sendMessage", smsArr); 
-//			System.out.println(result1[0]); 
-//			serviceFlag = true;
-//		} catch (MalformedURLException e) { 
-//			e.printStackTrace(); 
-//		} catch (Exception e) { 
-//			e.printStackTrace(); 
-//		}finally{
-//			return true;
-//		}
-//		
-//	}
-//	
-//	/**
-//	 * 查询短信状态
-//	 */
-//	public void searchSMSZT(){
-//		try {
-////			http://203.0.64.68/dxpt/services/SendSMS?wsdl
-//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SearchSMS?wsdl")); 
-//			Object[] result1 = client.invoke("getFszt", new Object[] {1, 2}); 
-//			System.out.println(result1[0]); 
-//		} catch (MalformedURLException e) { 
-//			e.printStackTrace(); 
-//		} catch (Exception e) { 
-//			e.printStackTrace(); 
-//		}
-//		
-//	}
-//	
-//	/**
-//	 * 删除未发送短信
-//	 * @return
-//	 */
-//	public boolean delSMS(){
-//		boolean serviceFlag = false;
-//		try {
-//			Client client = new Client(new URL("http://dxpt.zj.pcc/dxpt/services/SendSMS?wsdl")); 
-//			Object[] result1 = client.invoke("delSMS", new Object[] {1, 2}); 
-//			System.out.println(result1[0]); 
-//			serviceFlag = true;
-//		} catch (MalformedURLException e) { 
-//			e.printStackTrace(); 
-//		} catch (Exception e) { 
-//			e.printStackTrace(); 
-//		}finally{
-//			return true;
-//		}
-//	}
+	public String[][] sendSMS(SmsBean bean){
+		String[][] sendRet = null;
+		if(bean==null || bean.getFsr_userid()==null || bean.getJssjhm()==null 
+				|| bean.getJsrmc()==null || bean.getFsnr()==null){
+			//啥都不干
+		}else{
+			String[][] smsArr = new String[1][10];
+			smsArr[0][0] = bean.getXtbh();
+			smsArr[0][1] = bean.getFsr_fybm();
+			smsArr[0][2] = bean.getFsr_userid();
+			smsArr[0][3] = bean.getJssjhm();
+			smsArr[0][4] = bean.getJsrmc();
+			smsArr[0][5] = bean.getFsnr();
+			smsArr[0][6] = bean.getDsfssj();
+			smsArr[0][7] = bean.getFssm();
+			smsArr[0][8] = bean.getId2();
+			smsArr[0][9] = bean.getAh();
+			SendSMSClient smsClient  = new SendSMSClient();
+			sendRet = smsClient.SendMessage(smsArr, null);
+		}
+		return sendRet;
+//		返回数据也为二维数组：
+//		Arr[i][0] 业务系统短信息唯一id号
+//		Arr[i][1] 发送结果编号（01 发送成功；02 发送人不存在；03 接收人手机号码长度不为11位；04 接收人手机号码没有以指定数字开头 ；05 发送内容超过指定数字（目前限定为600） ；06 定时发送时间格式不准确，格式为2013-11-07 20:02:00；07 必输项填写不完整；08 外省号码；09 其他错误;10 发送内容中有多个'#'，可能是短信模板；11 短信重复，无法发送；12  业务系统短信唯一编号大于45位）
+	}
+	
+	
+	/**
+	 * 发送短信
+	 * @return
+	 */
+	public String[][] sendSMSList(List<SmsBean> smsList){
+		String[][] sendRet = null;
+		if(smsList!=null && smsList.size()>0){
+			String[][] smsArr = new String[smsList.size()][10];
+			for(int i =0;i<smsList.size();i++){
+				SmsBean bean = smsList.get(i);
+				smsArr[i][0] = bean.getXtbh();
+				smsArr[i][1] = bean.getFsr_fybm();
+				smsArr[i][2] = bean.getFsr_userid();
+				smsArr[i][3] = bean.getJssjhm();
+				smsArr[i][4] = bean.getJsrmc();
+				smsArr[i][5] = bean.getFsnr();
+				smsArr[i][6] = bean.getDsfssj();
+				smsArr[i][7] = bean.getFssm();
+				smsArr[i][8] = bean.getId2();
+				smsArr[i][9] = bean.getAh();
+			}
+			
+			SendSMSClient smsClient  = new SendSMSClient();
+			sendRet = smsClient.SendMessage(smsArr, null);
+		}
+		return sendRet;
+//		返回数据也为二维数组：
+//		Arr[i][0] 业务系统短信息唯一id号
+//		Arr[i][1] 发送结果编号（01 发送成功；02 发送人不存在；03 接收人手机号码长度不为11位；04 接收人手机号码没有以指定数字开头 ；05 发送内容超过指定数字（目前限定为600） ；06 定时发送时间格式不准确，格式为2013-11-07 20:02:00；07 必输项填写不完整；08 外省号码；09 其他错误;10 发送内容中有多个'#'，可能是短信模板；11 短信重复，无法发送；12  业务系统短信唯一编号大于45位）
+	}
+	
+	/**
+	 * 查询短信状态
+	 */
+	public String[][] searchSMSZT(String[] smsIds){
+		SearchSMSClient smsClient = new SearchSMSClient();
+		String[][] sendRet = smsClient.getFszt("023", smsIds);
+		return sendRet;
+	}
+	
+	/**
+	 * 删除未发送短信
+	 * @return
+	 */
+	public String[][] delSMS(String[] smsIds){
+		boolean serviceFlag = false;
+		SendSMSClient smsClient  = new SendSMSClient();
+		String[][] delRet = smsClient.delSMS("023", smsIds);
+		return delRet;
+	}
 	
 }
